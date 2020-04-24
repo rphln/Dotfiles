bdf424307b13e4ba5cf443611ef5a5874dbdea0e
Raphael Nepomuceno <58113327+rphln@users.noreply.github.com>
2020-04-24 16:42:51 -0300

    qmk(atreis): Add debouncing.

--- _qmk/keyboards/atreis/config.h
+++ _qmk/keyboards/atreis/config.h
@@ -94,7 +94,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is
  * not needed */
-#define DEBOUNCE 0
+#define DEBOUNCE (10)
 
 /* define if matrix has ghost (lacks anti-ghosting diodes) */
 //#define MATRIX_HAS_GHOST
