358404564440c206b4064b67ea6aad88e1ec9b30
Raphael Nepomuceno <58113327+rphln@users.noreply.github.com>
2020-05-08 21:17:10 -0300

    qmk(atreis): Minor refactor.

--- _qmk/keyboards/atreis/keymaps/rphln/keymap.c
+++ _qmk/keyboards/atreis/keymaps/rphln/keymap.c
@@ -33,18 +33,13 @@ enum custom_keycodes {
 
 #define KC_RST (RESET)
 
+#define KC_ADJ (MO(_ADJUST))
 #define KC_LOW (MO(_LOWER))
 #define KC_RAI (MO(_RAISE))
 
-#define KC_ARRW (DF(_RAISE))
-#define KC_NMPD (DF(_LOWER))
+#define KC_ARRW (DF(_LOWER))
+#define KC_NMPD (DF(_RAISE))
 #define KC_QWFP (DF(_COLEMAK))
-
-// Cycle keyboard layouts on Plasma.
-#define KC_KBD (LCTL(LALT(KC_K)))
-
-// `<M-F4>` is hard to reach.
-#define KC_QUIT (LALT(KC_F4))
 
 // `KC_SYSREQ` doesn't work.
 #define KC_SYSRQ (LALT(KC_PSCR))
@@ -57,49 +52,45 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // ├──────┼──────┼──────┼──────┼──────┼──────┤             ├──────┼──────┼──────┼──────┼──────┼──────┤
              ESC  , A    , R    , S    , T    , G    ,               M    , N    , E    , I    , O    , ENT  ,
         // ├──────┼──────┼──────┼──────┼──────┼──────┤             ├──────┼──────┼──────┼──────┼──────┼──────┤
-             LSPO , Z    , X    , C    , D    , V    ,               K    , H    , COMM , DOT  , SLSH , RSPC ,
+             LCPO , Z    , X    , C    , D    , V    ,               K    , H    , COMM , DOT  , SLSH , RCPC ,
         // ╰──────┴──────┴──────┴────┬─┴────┬─┴────┬─┴────╮   ╭────┴─┬────┴─┬────┴─┬────┴──────┴──────┴──────╯
-                                       LWIN , LOW  , LCTL ,     SPC  , RAI  , LALT
+                                       LWIN , LOW  , LSFT ,     SPC  , RAI  , LALT
         //                           ╰──────┴──────┴──────╯   ╰──────┴──────┴──────╯
         ),
     [_LOWER] = LAYOUT_kc(
         // ╭──────┬──────┬──────┬──────┬──────┬──────╮             ╭──────┬──────┬──────┬──────┬──────┬──────╮
-             DEL  , PGUP , HOME , UP   , END  ,      ,               PLUS , MINS , LCBR , RCBR , COLN , TILD ,
+             ADJ  , PGUP , HOME , UP   , END  , LCBR ,               RCBR , EQL  , TILD , PIPE , COLN , DEL  ,
         // ├──────┼──────┼──────┼──────┼──────┼──────┤             ├──────┼──────┼──────┼──────┼──────┼──────┤
-             INS  , PGDN , LEFT , DOWN , RGHT ,      ,               EQL  , UNDS , LBRC , RBRC , SCLN , GRV  ,
+             CAPS , PGDN , LEFT , DOWN , RGHT , LBRC ,               RBRC , UNDS , GRV  , BSLS , SCLN , INS  ,
         // ├──────┼──────┼──────┼──────┼──────┼──────┤             ├──────┼──────┼──────┼──────┼──────┼──────┤
-             _    ,      ,      , PSCR , MPLY ,      ,                    , INTO , BSLS , PIPE ,      , _    ,
+             _    ,      ,      , PSCR , MPLY ,      ,                    , INTO ,      ,      ,      , _    ,
         // ╰──────┴──────┴──────┴────┬─┴────┬─┴────┬─┴────╮   ╭────┴─┬────┴─┬────┴─┬────┴──────┴──────┴──────╯
-                                       _    , _    , LCTL ,     RCTL , _    , _
+                                            , LOW  ,      ,     RSFT , LWIN , LALT
         //                           ╰──────┴──────┴──────╯   ╰──────┴──────┴──────╯
         ),
     [_RAISE] = LAYOUT_kc(
         // ╭──────┬──────┬──────┬──────┬──────┬──────╮             ╭──────┬──────┬──────┬──────┬──────┬──────╮
              F12  , F1   , F2   , F3   , F4   , F5   ,               F6   , F7   , F8   , F9   , F10  , F11  ,
         // ├──────┼──────┼──────┼──────┼──────┼──────┤             ├──────┼──────┼──────┼──────┼──────┼──────┤
-                  , 1    , 2    , 3    , 4    , 5    ,               6    , 7    , 8    , 9    , 0    ,      ,
+             MINS , 1    , 2    , 3    , 4    , 5    ,               6    , 7    , 8    , 9    , 0    , PLUS ,
         // ├──────┼──────┼──────┼──────┼──────┼──────┤             ├──────┼──────┼──────┼──────┼──────┼──────┤
              _    , EXLM , AT   , HASH , DLR  , PERC ,               CIRC , AMPR , ASTR , LPRN , RPRN , _    ,
         // ╰──────┴──────┴──────┴────┬─┴────┬─┴────┬─┴────╮   ╭────┴─┬────┴─┬────┴─┬────┴──────┴──────┴──────╯
-                                       _    , _    , LCTL ,     RCTL , _    , _
+                                       LWIN , LALT , LSFT ,          , RAI  ,
         //                           ╰──────┴──────┴──────╯   ╰──────┴──────┴──────╯
         ),
     [_ADJUST] = LAYOUT_kc(
         // ╭──────┬──────┬──────┬──────┬──────┬──────╮             ╭──────┬──────┬──────┬──────┬──────┬──────╮
-             KBD  , OOMK ,      , VOLU ,      ,      ,                    ,      ,      ,      ,      , RST  ,
+                  ,      ,      ,      ,      ,      ,                    ,      , VOLU ,      ,      , RST  ,
         // ├──────┼──────┼──────┼──────┼──────┼──────┤             ├──────┼──────┼──────┼──────┼──────┼──────┤
-             CAPS , QUIT , MPRV , VOLD , MNXT ,      ,                    ,      ,      ,      ,      ,      ,
+                  ,      ,      ,      ,      ,      ,                    , MPRV , VOLD , MNXT ,      , OOMK ,
         // ├──────┼──────┼──────┼──────┼──────┼──────┤             ├──────┼──────┼──────┼──────┼──────┼──────┤
-             _    ,      , ARRW , NMPD , QWFP ,      ,                    ,      ,      ,      ,      , _    ,
+             _    ,      ,      ,      ,      ,      ,                    , QWFP , ARRW , NMPD ,      , _    ,
         // ╰──────┴──────┴──────┴────┬─┴────┬─┴────┬─┴────╮   ╭────┴─┬────┴─┬────┴─┬────┴──────┴──────┴──────╯
-                                       _    , _    , LCTL ,     RCTL , _    , _
+                                            ,      ,      ,          ,      ,
         //                           ╰──────┴──────┴──────╯   ╰──────┴──────┴──────╯
         ),
     // clang-format on
-};
-
-uint32_t layer_state_set_user(uint32_t state) {
-  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
