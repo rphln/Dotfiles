057b007677dbeb80caa70d5d5143c5efeda3f74d
Raphael Nepomuceno <58113327+rphln@users.noreply.github.com>
2020-05-12 10:33:02 -0300

    zsh(options): Unset `REPORTTIME`, update comments.

--- zsh/.config/zsh/options.zsh
+++ zsh/.config/zsh/options.zsh
@@ -1,13 +1,8 @@
-# Miscellaneous options.
-
 # Allow comments in interactive shells.
 setopt interactive_comments
 
-# Make prompt static.
+# Make the prompt static.
 unsetopt prompt_subst
 
-# Report duration for long-running commands.
-export REPORTTIME=5
-
-# Shows current directory and its parent.
+# Show the current directory and its parent.
 export PS1="%2~: "
