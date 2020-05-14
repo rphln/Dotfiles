b5785bf58e7e0d270a3e888d452293ce4cf59428
Raphael Nepomuceno <58113327+rphln@users.noreply.github.com>
2020-05-14 13:29:11 -0300

    fish(git): Update git abbreviations.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -12,26 +12,27 @@ alias ll 'l -l --human-readable'
 abbr v 'nvim'
 abbr f 'fg &>/dev/null'
 
-abbr gcm 'git checkout master'
-abbr gco 'git checkout'
-
-abbr gpf 'git push --force-with-lease'
-abbr gp! 'git push --force'
-
-abbr ga 'git add'
-abbr gc 'git commit --verbose'
-abbr gd 'git diff'
-abbr gl 'git pull'
-abbr gm 'git merge'
-abbr gr 'git rebase --interactive'
-abbr gs 'git status --short'
-
 abbr cp 'cp --interactive'
 abbr mv 'mv --interactive'
 abbr rm 'rm --interactive'
 
 abbr md 'mkdir --parents'
 abbr rd 'rmdir --parents'
+
+abbr ga 'git add'
+abbr gc 'git commit --verbose'
+abbr gd 'git diff'
+abbr gl 'git pull'
+abbr gm 'git merge'
+abbr gp 'git push'
+abbr gr 'git rebase --interactive'
+abbr gs 'git status --short'
+
+abbr gcm 'git checkout master'
+abbr gco 'git checkout'
+
+abbr gpf 'git push --force-with-lease'
+abbr gp! 'git push --force'
 
 if type -q hub
     alias git 'hub'
