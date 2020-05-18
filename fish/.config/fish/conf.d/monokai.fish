fd3765dc9dd66b0aea86b87c2307cf6c2cfc65ba
Raphael Nepomuceno <58113327+rphln@users.noreply.github.com>
2020-05-18 12:22:02 -0300

    fish(colors): Set the highlight colors.

---
+++ fish/.config/fish/conf.d/monokai.fish
@@ -0,0 +1,19 @@
+set fish_color_normal normal
+set fish_color_param normal
+set fish_color_comment black
+set fish_color_command blue
+set fish_color_cwd cyan
+set fish_color_escape cyan
+set fish_color_match green
+set fish_color_end magenta
+set fish_color_operator magenta
+set fish_color_redirection magenta
+set fish_color_search_match normal --background green
+set fish_color_error normal --background red
+set fish_color_quote yellow
+
+set fish_pager_color_completion black
+set fish_pager_color_description black
+set fish_pager_color_prefix normal
+set fish_pager_color_progress white
+set fish_pager_color_secondary white
