85e475163c43d31e96cf5d111947181fc915cfac
Raphael Nepomuceno <58113327+rphln@users.noreply.github.com>
2021-04-06 06:41:11 -0300

    fish: Fix `ls`.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -81,7 +81,7 @@ bind \co 'browse-here'
 set -U fish_features '3.0'
 set -U fish_greeting
 
-alias ls 'command ls --color --classify --group-directories-first'
+alias ls '\ls --color --classify --group-directories-first'
 
 function chpwd --on-variable PWD
     ls
