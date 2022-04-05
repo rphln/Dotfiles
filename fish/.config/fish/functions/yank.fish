2ab0b544b9bf36b719cbaae5d576e9aac118d488
Raphael Nepomuceno <rphln@users.noreply.github.com>
2022-04-05 08:46:27 -0300

    fish: Add clipboard fallbacks to `yank`.

--- fish/.config/fish/functions/yank.fish
+++ fish/.config/fish/functions/yank.fish
@@ -1,3 +1,15 @@
 function yank --description "Pipes a value into the clipboard."
-    perl -pe 'chomp if eof' | base64 --wrap 0 | xargs printf '\e]52;c;%s;\a'
+    perl -pe 'chomp if eof' | if type --query pbcopy
+        pbcopy
+    else if type --query wl-copy && set --query WAYLAND_DISPLAY
+        wl-copy
+    else if type --query xsel && set --query DISPLAY
+        xsel --input --clipboard
+    else if type --query xclip && set --query DISPLAY
+        xclip -selection clipboard
+    else if type --query clip.exe
+        clip.exe
+    else
+        base64 --wrap 0 | xargs printf '\e]52;c;%s;\a'
+    end
 end
