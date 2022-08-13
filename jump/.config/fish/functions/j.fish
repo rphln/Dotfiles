2bef27e19ffb7e72f3c3f55d17f87f7a7357491b
Raphael Nepomuceno <rphln@users.noreply.github.com>
2022-08-13 16:49:05 -0300

    jump: Don't print the matched directory.
    We already get to see it in the prompt anyway.

--- jump/.config/fish/functions/j.fish
+++ jump/.config/fish/functions/j.fish
@@ -2,7 +2,6 @@ function j
     set --local target (jump query $argv)
 
     if [ -n "$target" ]
-        printf "%s→%s %s\n" (set_color green) (set_color normal) $target
         cd -- "$target"
     else
         echo >&2 "The query `$argv` did not yield any results."
