aaa82559197d1786e3b4dba3a1e5bc93de8dc40a
Raphael Nepomuceno <rphln@users.noreply.github.com>
2022-08-14 12:16:21 -0300

    fish: Set pager colors.

---
+++ fish/.config/fish/conf.d/less.fish
@@ -0,0 +1,7 @@
+set --export LESS_TERMCAP_mb (set_color red) # Begin blinking.
+set --export LESS_TERMCAP_md (set_color blue) # Begin bold.
+set --export LESS_TERMCAP_me (set_color normal) # End mode.
+set --export LESS_TERMCAP_se (set_color normal) # End standout-mode.
+set --export LESS_TERMCAP_so (set_color black) # Begin standout-mode.
+set --export LESS_TERMCAP_ue (set_color normal) # End underline.
+set --export LESS_TERMCAP_us (set_color green) # Begin underline.
